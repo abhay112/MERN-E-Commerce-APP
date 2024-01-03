52473ee4c7c97cf51974d1a9c48599c7ec2fed6b
abhay112 <sattibhai501@gmail.com>
2024-01-03 14:47:22 +0530

    add user and view all user working

--- ecommerce-backend/dist/app.js
+++ ecommerce-backend/dist/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+// Importting Routes
+import userRoutes from './routes/user.js';
+import { connectDB } from "./utils/features.js";
 const port = 3000;
+connectDB();
 const app = express();
+app.use(express.json());
+//Using Routes
+app.use("/api/v1/user", userRoutes);
+app.get("/", (req, res) => {
+    res.send("Api Working with /api/v1");
+});
 app.listen(port, () => {
     console.log(`started ${port}`);
 });
---
+++ ecommerce-backend/src/routes/user.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import {
+    newUser,
+    getAllUsers
+} from "../controllers/user.js";
+const app = express.Router();
+
+// route - /api/v1/user/new
+app.post("/new", newUser);
+
+// Route - /api/v1/user/all
+app.get("/all",getAllUsers);
+
+// Route - /api/v1/user/dynamicID
+app.route("/:id").get().delete();
+
+export default app;
